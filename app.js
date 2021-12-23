473ffbf72fea6fd0ee7fe3a42af179e4477c3583
Charlie <81655655+TheNightSky@users.noreply.github.com>
2021-12-23 15:15:07 -0500

    fixed routing

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+let port = process.env.PORT;
 const express = require('express');
 const app = express();
 const path = require('path')
 const ejsMate = require('ejs-mate');
+
+
+
 
 
 app.engine('ejs', ejsMate);
@@ -15,18 +19,22 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 
 app.get('/', (req, res) => {
-    res.render('index')
+  res.render('index')
 });
 
 app.get("/srp",(req, res) => {
-    res.render('srp');
+  res.render('srp');
+});
+
+app.get("/downloadCv", (req, res) => {
+  res.download("views/styles/uploads/CV-2021-September.pdf");
 });
 
 
-
-
-let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
-}
+};
 app.listen(port);
+
+
+
